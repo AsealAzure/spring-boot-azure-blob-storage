4aac1b81376764521c3704e8857d7f132ca204ea
Abhijit Seal <abhijit.seal@blueyonder.com>
2020-05-25 11:04:41 +0530

    Initial Commit

---
+++ src/main/java/com/as/azure/storage/StorageApplication.java
@@ -0,0 +1,12 @@
+package com.as.azure.storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StorageApplication.class);
+    }
+}
